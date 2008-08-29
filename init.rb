9f0cce77dbd7a69334e0ffc682b1a27784a02d1c
Roberto Salicio <roberto.salicio@gmail.com>
2008-08-29 11:07:35 +0200

    Ticket #62. can't register: mailer fails building activation url

--- init.rb
+++ init.rb
@@ -5,4 +5,6 @@ Tog::Plugins.settings :tog_core,  'language.default'          => "en",
                                   'host.port'                 => 3000,
                                   'mail.system_from_address'  => "Tog Admin <tog@linkingpaths.com>",
                                   'mail.default_subject'      => "[Tog Community] "
-                                  
+
+
+require 'lib/url_writer_retardase_inhibitor'
