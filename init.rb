1c1d3fb267dea3358fe7e657ed006896aa7ca521
Roberto Salicio <roberto.salicio@gmail.com>
2008-08-29 11:59:31 +0200

    Fix require path

--- init.rb
+++ init.rb
@@ -6,5 +6,4 @@ Tog::Plugins.settings :tog_core,  'language.default'          => "en",
                                   'mail.system_from_address'  => "Tog Admin <tog@linkingpaths.com>",
                                   'mail.default_subject'      => "[Tog Community] "
 
-
-require 'lib/url_writer_retardase_inhibitor'
+require "#{File.dirname(__FILE__)}/lib/url_writer_retardase_inhibitor"
