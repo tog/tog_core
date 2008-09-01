bda777f8e26b52d26a41c9b76b45f9713a02aede
Roberto Salicio <roberto.salicio@gmail.com>
2008-09-01 16:57:45 +0200

    include core helper

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_filter :set_javascripts_and_stylesheets
 
+  helper :core
+
   def set_javascripts_and_stylesheets
     @javascripts = %w(prototype effects dragdrop controls application)
     @stylesheets = %w(base)
