7dcb484758608c5c99ffa6d12ec38eede4dcf6d4
Roberto Salicio <roberto.salicio@gmail.com>
2008-09-11 17:32:00 +0200

    Patch ActionView::Base.field_error_proc, changed html to <span class="field_with_errors">#{html_tag}</span>

--- init.rb
+++ init.rb
@@ -5,11 +5,13 @@ Tog::Plugins.settings :tog_core,  'language.default'          => "en",
                                   'host.port'                 => 3000,
                                   'site.name'                 => "toginstallation.com",                                  
                                   'mail.system_from_address'  => "Tog Admin <tog@linkingpaths.com>",
-                                  'mail.default_subject'      => "[Tog Installation] "
+                                  'mail.default_subject'      => "[Tog Installation] ",
+                                  'patch_field_error_proc'    => true
 
 require "url_writer_retardase_inhibitor"
 require "acts_as_commentable_patch"
 require "will_paginate"
+require "active_record_helper_patch"
 
 Tog::Interface.sections(:admin).add "Home", "/admin"          
 Tog::Interface.sections(:member).add "Home", "/"     
