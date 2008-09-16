81a196ab2a68a48117cf9277fff2ffd155a89c67
Aitor García <aitor.garcia@gmail.com>
2008-09-16 19:56:41 +0200

    Revert "Extracted the funcionality to reload certain plugins to its own patch. Needed for acts_as_commentable and acts_as_scribe"
    This reverts commit c5d8ed2db4ec64a5853d8920f7f9c624d9300abe.

--- lib/reloadable_plugins_patch.rb
+++
@@ -1,8 +0,0 @@
-# Array of plugins with Application model dependencies.
-reloadable_plugins = ["acts_as_commentable", "acts_as_scribe"]
-
-# Force these plugins to reload, avoiding stale object references.
-reloadable_plugins.each do |plugin_name|
-  reloadable_path = RAILS_ROOT + "/vendor/plugins/#{plugin_name}/lib"
-  ActiveSupport::Dependencies.load_once_paths.delete(reloadable_path)
-end
