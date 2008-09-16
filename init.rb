683081405f6540d5f7be54818f7705f602f2f1c3
Aitor García <aitor.garcia@gmail.com>
2008-09-16 18:19:42 +0200

    Conditional I18n require on tog_core

--- init.rb
+++ init.rb
@@ -13,6 +13,18 @@ require "acts_as_commentable_patch"
 require "will_paginate"
 require "active_record_helper_patch"
 
+
+# We've backported i18n-0.0.1 from rails to Tog, allowing pre-Rails 2.2 to use Backend::Simple
+# until they're upgraded is released and we see widespread adoption of it.
+require "i18n" unless defined?(I18n)
+
+
+Dir[File.dirname(__FILE__) + '/locale/**/*.yml'].each do |file|
+  I18n.load_translations file
+end
+
+
+
 Tog::Interface.sections(:admin).add "Home", "/admin"          
 Tog::Interface.sections(:member).add "Home", "/"     
 Tog::Interface.sections(:site).add "Home", "/"     
