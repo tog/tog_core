168fe502621fc37c6d3d04c83cd65a32cdecf844
Aitor García <aitor.garcia@gmail.com>
2008-09-16 20:10:07 +0200

    Extracted the funcionality to reload certain plugins to its own patch. Needed for acts_as_commentable and acts_as_scribe

--- init.rb
+++ init.rb
@@ -1,4 +1,9 @@
 require_plugin 'acts_as_commentable'
+require_plugin 'acts_as_scribe'
+
+# require the will_paginate as a gem. This could be ´config.gem´ as well.
+require "will_paginate"
+
 
 Tog::Plugins.settings :tog_core,  'language.default'          => "en",
                                   'host.name'                 => "0.0.0.0",
@@ -8,17 +13,15 @@ Tog::Plugins.settings :tog_core,  'language.default'          => "en",
                                   'mail.default_subject'      => "[Tog Installation] ",
                                   'patch_field_error_proc'    => true
 
-require "url_writer_retardase_inhibitor"
-require "acts_as_commentable_patch"
-require "will_paginate"
 require "active_record_helper_patch"
+require "acts_as_commentable_patch"
+require "reloadable_plugins_patch"
+require "url_writer_retardase_inhibitor"
 
 
 # We've backported i18n-0.0.1 from rails to Tog, allowing pre-Rails 2.2 to use Backend::Simple
 # until they're upgraded is released and we see widespread adoption of it.
 require "i18n" unless defined?(I18n)
-
-
 Dir[File.dirname(__FILE__) + '/locale/**/*.yml'].each do |file|
   I18n.load_translations file
 end
