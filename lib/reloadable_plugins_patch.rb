50e3a6ea6aa624f0abe63cbb7a79dce37a3a5613
Roberto Salicio <roberto.salicio@gmail.com>
2008-09-17 12:36:14 +0200

    added acts_as_taggable as a reloadable_plugin

--- lib/reloadable_plugins_patch.rb
+++ lib/reloadable_plugins_patch.rb
@@ -1,5 +1,5 @@
 # Array of plugins with Application model dependencies.
-reloadable_plugins = ["acts_as_commentable", "acts_as_scribe"]
+reloadable_plugins = ["acts_as_commentable", "acts_as_scribe", "acts_as_taggable"]
 
 # Force these plugins to reload, avoiding stale object references.
 reloadable_plugins.each do |plugin_name|
