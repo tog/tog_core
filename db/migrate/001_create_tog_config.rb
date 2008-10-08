c3913da4def50244e25d12d3b0b1040addd2cc02
Aitor García <aitor.garcia@gmail.com>
2008-10-08 12:11:05 +0200

    Since we've had problems with the proper moment to create the config table we've moved the process of manage the table existence to a internal method

--- db/migrate/001_create_tog_config.rb
+++ db/migrate/001_create_tog_config.rb
@@ -1,7 +1,7 @@
 class CreateTogConfig < ActiveRecord::Migration
   def self.up
     # Since we've had problems with the proper moment to create the config table
-    # We've moved the process of manage the table existence to a internal method
+    # we've moved the process of manage the table existence to a internal method
     # Called create_tog_config_table on Tog::Config. So for now... nothing to see
     # here. Move on. Please. Now.
   end
