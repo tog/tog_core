727b238f00065ea325b5e134076b4ce3f800e56c
Aitor García <aitor.garcia@gmail.com>
2008-10-23 19:37:22 +0200

    Tog::Search improved interface and docss

--- app/models/tog/search.rb
+++ app/models/tog/search.rb
@@ -1,39 +1,50 @@
 require 'simpleton'
 module Tog
+  #
+  # Tog::Search provides a simple interface to site-wide searches through a flexible collection of +sources+
+  # that the developer can extend to plug its own classes on the general search.
+  #
+  # ==== Simple example
+  #
+  #   Tog::Search.sources << User
+  #   @results = Tog::Search.search("joe-theplumber") # the @results instance will be paginated
+  #
+  # The modules used as +sources+ should implement a class method called search(query, search_options={}) and
+  # should return an +Array+ instance.
   class Search
     include Simpleton
     attr_accessor :sources
-    
+
     def initialize
       @sources = []
     end
 
-    def search(query)
+    # This method iterates all the classes/modules (usually activerecord models) added to the +sources+ attribute.
+    # For each class, its search method is called and the matches are added to the paginated collection of 
+    # final results.
+    #
+    # ==== Attributes
+    #
+    # * <tt>:search_options</tt> - search options.
+    # * <tt>:paginate_options</tt> - The same options allowed for the +paginate+ method provided by will_paginate
+    #
+    def search(query, search_options = {}, paginate_options = {})
       results = []
       @sources.each{|source|
-        results << source.search(query) if source.respond_to? :search
+        if source.respond_to?(:search)
+          results << source.search(query, search_options)
+        else
+          RAILS_DEFAULT_LOGGER.warn(<<-WARNING
+
+*********************************************************************************************************************************************
+SEARCH WARNING: The source #{source} should implement a ´self.search(query, search_options={})´ method to be available on site-wide searches.
+*********************************************************************************************************************************************
+
+          WARNING
+          )
+        end
       }
-      results
-    end
-  end
-  class SearchResult
-    attr_accessor :klass, :id, :title, :description
-    
-    def initialize(klass, id, title, description)
-      @klass, @id, @title, @description = klass, id, title, description
+      results.flatten.paginate(paginate_options)
     end
   end
 end
-
-
-#Tog::Search.sources << User
-#
-#@results = Tog::Search.search("ipod")
-#
-#class Course << ActiveRecord::Base
-#  def search(query)
-#    results = []
-#    users = User.find(all, ['name LIKE ?', "%#{query}%"])
-#    users.each {|user| }
-#  end
-#end
