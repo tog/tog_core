72364ec589c4ccfcb7c943184bd091c2dddfff70
Aitor García <aitor.garcia@gmail.com>
2008-12-11 12:55:06 +0100

    pagination fixed on search result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
 
   def search
+    @page = params[:page] || '1'
     @term = params[:global_search_field]
-    @matches = Tog::Search.search(@term)
+    @matches = Tog::Search.search(@term, {}, {:page => @page})
     respond_to do |format|
       format.html 
       format.xml  { render :xml => @matches }
