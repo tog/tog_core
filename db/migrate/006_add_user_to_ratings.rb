a720bf85fa3a826075e30a6abfdfa5686a8ef88e
Roberto Salicio <roberto.salicio@gmail.com>
2009-01-08 19:10:24 +0100

    only add user column

---
+++ db/migrate/006_add_user_to_ratings.rb
@@ -0,0 +1,9 @@
+class AddUserToRatings < ActiveRecord::Migration
+  def self.up
+    add_column :ratings, :user_id, :integer
+  end
+
+  def self.down
+    remove_column :ratings, :user_id
+  end
+end
