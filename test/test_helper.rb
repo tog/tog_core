75fa4bcb20c1a15f53c5fef72255f4528ec07e64
aitor <aitor@linkingpaths.com>
2009-06-17 04:51:13 -0700

    Gem name fixed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require 'action_view/test_case'
 require 'mocha'
 
 begin
-  gem 'shoulda', '>=2.10.1'
+  gem 'thoughtbot-shoulda', '>=2.10.1'
   require 'shoulda' 
 rescue Exception => e
   puts "\n\nYou need shoulda 2.10.1 or greater to test tog_core. Visit http://github.com/thoughtbot/shoulda to view details.\n\n" 
