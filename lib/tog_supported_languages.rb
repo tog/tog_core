e07b73be87a6af54c8fe2aa1f6e1904f615d5c07
Rafael <rafael@Macintosh.local>
2009-10-29 07:46:17 +0800

    New languade added: Brazilian Portuguese

--- lib/tog_supported_languages.rb
+++ lib/tog_supported_languages.rb
@@ -1,6 +1,7 @@
 module TogSupportedLanguages
   LANGUAGES = { 
     :es => {:iso_639_1 => "es", :iso_639_2 => "spa",:name => "Spanish", :native_name => "Español" },
-    :en => {:iso_639_1 => "en", :iso_639_2 => "eng",:name => "English", :native_name => "English" }
+    :en => {:iso_639_1 => "en", :iso_639_2 => "eng",:name => "English", :native_name => "English" },
+    :pt_BR => {:iso_639_1 => "pt-BR", :iso_639_2 => "por",:name => "Brazilian Portuguese", :native_name => "Português Brasileiro" }
   }
 end
